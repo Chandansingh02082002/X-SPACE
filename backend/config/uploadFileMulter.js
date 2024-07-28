fa3f1f7f22bf9293600acc5e84fa52a0c6cb0ede
ChandanSinghChauhan <chandansinghchauhan2002@gmail.com>
2024-07-28 13:48:28 +0530

    mutler code update

--- backend/config/uploadFileMulter.js
+++ backend/config/uploadFileMulter.js
@@ -1,14 +1,20 @@
 const multer = require("multer");
 
 // Set up storage for uploaded files
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, "./uploads/");
-    },
-    filename: (req, file, cb) => {
-        cb(null, `${Date.now()}-${Math.floor(Math.random() * 100)}-${file.originalname}`);
-    },
-});
+// const storage = multer.diskStorage({
+//     destination: (req, file, cb) => {
+//         cb(null, "./uploads/");
+//     },
+//     filename: (req, file, cb) => {
+//         cb(null, `${Date.now()}-${Math.floor(Math.random() * 100)}-${file.originalname}`);
+//     },
+// });
 
 // Create the multer instance
+const storage = new multer.memoryStorage();
+
+
+
+// Create the multer instance
+
 module.exports = multer({ storage: storage });
